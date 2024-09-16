52b427e72c13410bd923275ca6331662f8f6e59e
kvitrvn <kvitrvn@proton.me>
2024-09-16 23:15:10 +0200

    chore(ux): add basic ux elements

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,9 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Dropdown
+import { Dropdown } from "bootstrap";
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const _ = [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
